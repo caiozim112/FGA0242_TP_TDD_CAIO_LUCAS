a30ce3d94002aab86210755990acfef238b37d66
luclopesr <luclopesr@gmail.com>
2024-07-01 21:59:15 -0300

    test: teste parametrizado - EnderecoTest

--- demo/src/test/java/com/example/EnderecoTest.java
+++ demo/src/test/java/com/example/EnderecoTest.java
@@ -1,14 +1,38 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class EnderecoTest {
+    private String estado;
+    private boolean capital;
+
+    public EnderecoTest(String estado, boolean capital) {
+        this.estado = estado;
+        this.capital = capital;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"DF", true},
+                {"SP", false},
+                {"RJ", true},
+                {"MG", false}
+        });
+    }
 
     @Test
     public void testCadastroEndereco() {
-        Endereco endereco = new Endereco("DF", true);
-        assertEquals("DF", endereco.getEstado());
-        assertTrue(endereco.isCapital());
+        Endereco endereco = new Endereco(estado, capital);
+        assertEquals(estado, endereco.getEstado());
+        assertEquals(capital, endereco.isCapital());
     }
 }
