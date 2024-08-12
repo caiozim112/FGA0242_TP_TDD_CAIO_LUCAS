47725b139ae10167437b407f5f40f67e3658387b
luclopesr <luclopesr@gmail.com>
2024-08-12 00:28:44 -0300

    feat: extract method from Produto

---
+++ demo/src/main/java/com/example/Preco.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Preco {
+    private double valor;
+
+    public Preco(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
--- demo/src/main/java/com/example/Produto.java
+++ demo/src/main/java/com/example/Produto.java
@@ -3,13 +3,13 @@ package com.example;
 public class Produto {
     private int codigo;
     private String descricao;
-    private double valor;
+    private Preco preco;
     private String unidade;
 
-    public Produto(int codigo, String descricao, double valor, String unidade) {
+    public Produto(int codigo, String descricao, Preco preco, String unidade) {
         this.codigo = codigo;
         this.descricao = descricao;
-        this.valor = valor;
+        this.preco = preco;
         this.unidade = unidade;
     }
 
@@ -22,8 +22,13 @@ public class Produto {
     }
 
     public double getValor() {
-        return valor;
+        return preco.getValor();
     }
+
+    /*
+    Comentário (Trabalho 2: Refatoração):
+        A classe "Produto" foi refatorada para mover o atributo valor para uma nova classe "Preco". Isso permite que a lógica relacionada ao preço seja isolada em uma classe específica, facilitando a modificação e extensão, como aplicar diferentes tipos de descontos ou taxas.
+    */
 
     public String getUnidade() {
         return unidade;
